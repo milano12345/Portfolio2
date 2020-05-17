856db2904593969296fc1cd808fb45c1d887d027
milano12345 <dmilano857@gmail.com>
2020-05-17 15:24:06 -0500

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -203,8 +203,8 @@ function myFunction() {
     document.getElementById("frontend").className = "slideRight";
   }
   if (
-    document.body.scrollTop > 2000 ||
-    document.documentElement.scrollTop > 2000
+    document.body.scrollTop > 1600 ||
+    document.documentElement.scrollTop > 1600
   ) {
     document.getElementById("backend").className = "slideLeft";
   }
